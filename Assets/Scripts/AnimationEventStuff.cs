7c1985a3fef7cc983563a5de1abb740db4ef4cc6
a23patkr <patrikkroon1@gmail.com>
2024-01-27 21:50:09 +0100

    Update AnimationEventStuff.cs

--- Assets/Scripts/AnimationEventStuff.cs
+++ Assets/Scripts/AnimationEventStuff.cs
@@ -6,15 +6,30 @@ public class AnimationEventStuff : MonoBehaviour
 {
     [SerializeField] Animator animator;
 
+    GameObject player;
+
+    Vector2 speed;
+
+    [SerializeField] string speedName;
+
+
+    public void Awake()
+    {
+        player = GameObject.FindWithTag("Player");
+        speed = player.GetComponent<Rigidbody2D>().velocity;
+    }
+
     public void triggerValue(string variableName)
     {
         animator.SetTrigger(variableName);
     }
 
-    public void speedValue(string variableName, float speedValue)
+    public void Update()
     {
-        animator.SetFloat(variableName, speedValue);
-        
+        if (speed.x > 0 || speed.y > 0)
+        {
+            animator.SetFloat(speedName, Mathf.Sqrt(speed.x * speed.x + speed.y * speed.y));
+        }
     }
-    
+
 }
